30b206a3ba183c180c0ab80836b38f6ba4144f6a
David <davidamedina619@gmail.com>
2021-06-28 15:56:15 -0400

    init

---
+++ src/Intern_Challenge/Components/Display.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+
+function Display() {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default Display;
---
+++ src/Intern_Challenge/Components/Logic.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Display from './Display';
+
+
+
+function Logic() {
+  return (
+    <div>
+      <Display />
+    </div>
+  );
+}
+
+export default Logic;
